992e7ec579fb2a9345f03b02d5d6ae0a0966852a
quinceinc <quinceintelligence@gmail.com>
2026-03-22 21:57:20 -0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,10 @@
+import Link from "next/link";
 
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>QuincePay</h1>
+      <Link href="/login">Login</Link>
+    </main>
+  );
+}
