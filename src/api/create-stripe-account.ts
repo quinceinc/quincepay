5bed344472ec451eb030d253c97345547f2686fb
quinceinc <quinceintelligence@gmail.com>
2026-03-22 22:00:09 -0200

    Update create-stripe-account.ts

--- src/api/create-stripe-account.ts
+++ src/api/create-stripe-account.ts
@@ -1,1 +1,18 @@
+import { stripe } from "../../lib/stripe";
+import { supabaseAdmin } from "../../lib/supabaseAdmin";
 
+export default async function handler(req, res) {
+  const { userId, email } = JSON.parse(req.body);
+
+  const account = await stripe.accounts.create({
+    type: "express",
+    email,
+  });
+
+  await supabaseAdmin
+    .from("users")
+    .update({ stripe_account_id: account.id })
+    .eq("id", userId);
+
+  res.json({ accountId: account.id });
+}
