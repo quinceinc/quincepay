8ac7393d48656cc90161bd0bc3cc2aaba41fbe6d
quinceinc <quinceintelligence@gmail.com>
2026-03-22 22:17:05 -0200

    Update StatsCard.tsx

--- src/components/dashboard/StatsCard.tsx
+++ src/components/dashboard/StatsCard.tsx
@@ -1,1 +1,10 @@
+import Card from "../ui/Card";
 
+export default function StatsCard({ title, value }) {
+  return (
+    <Card>
+      <p className="text-gray-400">{title}</p>
+      <h3 className="text-2xl mt-2 font-semibold">{value}</h3>
+    </Card>
+  );
+}
