1ae7fd5b301a46edcf670dae8d55a6df910e2d4e
quinceinc <quinceintelligence@gmail.com>
2026-03-22 22:16:23 -0200

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,1 +1,14 @@
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 
+export default function Layout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 p-10">
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
+}
