8f85249227096b7ca6606b7bc3ec12b489f9ed08
quinceinc <quinceintelligence@gmail.com>
2026-03-22 22:14:44 -0200

    Update Input.tsx

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,1 +1,8 @@
-
+export default function Input(props) {
+  return (
+    <input
+      {...props}
+      className="w-full p-3 rounded-xl bg-white/5 border border-white/10 focus:outline-none"
+    />
+  );
+}
