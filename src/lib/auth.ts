e1bde42fde4e66685a1049adfbadc2c4286f0116
quinceinc <quinceintelligence@gmail.com>
2026-03-23 00:02:34 -0200

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,6 @@
-import { supabaseAdmin } from "./supabaseAdmin";
+import { supabase } from "./supabase";
 
-export async function getUser(req: any) {
-  const token = req.headers.authorization?.replace("Bearer ", "");
-
-  if (!token) return null;
-
-  const {
-    data: { user },
-  } = await supabaseAdmin.auth.getUser(token);
-
-  return user || null;
+export async function getUser() {
+  const { data: { session } } = await supabase.auth.getSession();
+  return session?.user || null;
 }
